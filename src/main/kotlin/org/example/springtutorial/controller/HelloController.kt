8e4acec871668b9384b078727277fe8b00d9fe12
Hanjoo Kim <hanjoo0211@gmail.com>
2025-10-10 23:38:24 +0900

    Feat: Section 3

--- src/main/kotlin/org/example/springtutorial/controller/HelloController.kt
+++ src/main/kotlin/org/example/springtutorial/controller/HelloController.kt
@@ -3,6 +3,8 @@ package org.example.springtutorial.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HelloController {
@@ -12,4 +14,24 @@ class HelloController {
         model.addAttribute("data", "hello^^!!")
         return "hello"
     }
+
+    @GetMapping("hello-mvc")
+    fun helloMvc(@RequestParam("name") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "hello-template"
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    fun helloString(@RequestParam("name") name: String): String {
+        return "hello $name"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    fun helloApi(@RequestParam("name") name: String): Hello {
+        return Hello(name)
+    }
+
+    class Hello(var name: String)
 }
